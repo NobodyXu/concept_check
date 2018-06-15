08c3d802023d25db822cfb1190bd070cb312abbd
NobodyXu <johnxujiahao2000@gmail.com>
2018-06-16 09:49:20 +1000

    Add add_reference.hpp.

---
+++ src/add_reference.hpp
@@ -0,0 +1,26 @@
+#ifndef __nobodyxu_concept_check_add_reference_HPP__
+# define __nobodyxu_concept_check_add_reference_HPP__
+namespace nxwheels::concept_check {
+template <class T>
+struct add_lvalue_reference {
+    typedef T& type;
+};
+template <class T>
+struct add_lvalue_reference<T&> {
+    typedef T& type;
+};
+template <class T>
+using add_lvalue_reference_t = typename add_lvalue_reference<T>::type;
+
+template <class T>
+struct add_rvalue_reference {
+    typedef T&& type;
+};
+template <class T>
+struct add_rvalue_reference<T&> {
+    typedef T&& type;
+};
+template <class T>
+using add_rvalue_reference_t = typename add_rvalue_reference<T>::type;
+} /* nxwheels::concept_check */
+#endif
---
+++ test/test_add_reference.cc
@@ -0,0 +1,76 @@
+#include <type_traits>
+#include "../src/add_reference.hpp"
+using namespace nxwheels::concept_check;
+
+template <class T1, class T2>
+struct assert_same {
+    static_assert(std::is_same<T1, T2>{}());
+};
+template <class T1, class T2>
+struct assert_add_lvalue_ref_t: assert_same<T1, add_lvalue_reference_t<T2>> {};
+template <class T1, class T2>
+struct assert_add_rvalue_ref_t: assert_same<T1, add_rvalue_reference_t<T2>> {};
+
+template <class T>
+constexpr void test_add_lvalue_reference_t() noexcept {
+    assert_add_lvalue_ref_t<T&, T> a1{};
+    assert_add_lvalue_ref_t<T&, T&> a2{};
+    assert_add_lvalue_ref_t<T&, T&&> a3{};
+
+    assert_add_lvalue_ref_t<const T&, const T> a4{};
+    assert_add_lvalue_ref_t<const T&, const T&> a5{};
+    assert_add_lvalue_ref_t<const T&, const T&&> a6{};
+
+    assert_add_lvalue_ref_t<volatile T&, volatile T> a7{};
+    assert_add_lvalue_ref_t<volatile T&, volatile T&> a8{};
+    assert_add_lvalue_ref_t<volatile T&, volatile T&&> a9{};
+
+    assert_add_lvalue_ref_t<const volatile T&, const volatile T> a10{};
+    assert_add_lvalue_ref_t<const volatile T&, const volatile T&> a11{};
+    assert_add_lvalue_ref_t<const volatile T&, const volatile T&&> a12{};
+}
+template <class T>
+constexpr void test_add_rvalue_reference_t() noexcept {
+    assert_add_rvalue_ref_t<T&&, T> a1{};
+    assert_add_rvalue_ref_t<T&&, T&> a2{};
+    assert_add_rvalue_ref_t<T&&, T&&> a3{};
+
+    assert_add_rvalue_ref_t<const T&&, const T> a4{};
+    assert_add_rvalue_ref_t<const T&&, const T&> a5{};
+    assert_add_rvalue_ref_t<const T&&, const T&&> a6{};
+
+    assert_add_rvalue_ref_t<volatile T&&, volatile T> a7{};
+    assert_add_rvalue_ref_t<volatile T&&, volatile T&> a8{};
+    assert_add_rvalue_ref_t<volatile T&&, volatile T&&> a9{};
+
+    assert_add_rvalue_ref_t<const volatile T&&, const volatile T> a10{};
+    assert_add_rvalue_ref_t<const volatile T&&, const volatile T&> a11{};
+    assert_add_rvalue_ref_t<const volatile T&&, const volatile T&&> a12{};
+}
+
+template <class T>
+constexpr void _test_add_reference() noexcept {
+    test_add_lvalue_reference_t<T>();
+    test_add_rvalue_reference_t<T>();
+}
+template <class T>
+constexpr void test_add_reference() noexcept {
+    _test_add_reference<T>();
+    _test_add_reference<T*>();
+}
+
+struct Foo {
+    int i;
+    void Func() {}
+};
+
+int main() {
+    test_add_reference<char>();
+    test_add_reference<float>();
+    test_add_reference<Foo>();
+
+    _test_add_reference<void*>();
+    _test_add_reference<void (*)()>();
+    _test_add_reference<int Foo::*>();
+    _test_add_reference<int (Foo::*)()>();
+}
