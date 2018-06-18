7dc31ea2ecd68275c71da2519280e5a3aab627b9
NobodyXu <johnxujiahao2000@gmail.com>
2018-06-18 21:37:35 +1000

    Get rid of dependency on std headers.

--- src/meta/type_bundles.hpp
+++ src/meta/type_bundles.hpp
@@ -1,14 +1,13 @@
 #ifndef __nobodyxu_wheels_meta_type_bundles_HPP__
 # define __nobodyxu_wheels_meta_type_bundles_HPP__
-
-namespace nxwheels::concept_check::meta::impl {
-template <class T> using pointer = T*;
-} /* nxwheels::concept_check::meta::impl */
+# include "../usefull_types.hpp"
+# include "../integral_constant.hpp"
+# include "../utility.hpp"
 
 # include <type_traits>
 namespace nxwheels::concept_check::meta {
 template <class ...Types>
-struct type_bundles: std::integral_constant<std::size_t, sizeof...(Types)> {
+struct type_bundles: integral_constant<size_t, sizeof...(Types)> {
     template <template <class...> class Tuple>
     constexpr type_bundles(Tuple<Types...>*) {}
 
@@ -20,12 +19,12 @@ template <template <class...> class Tuple, class ...Types>
 type_bundles(Tuple<Types...>*) -> type_bundles<Types...>;
 
 template <class Tuple>
-using type_bundles_t = decltype(type_bundles(impl::pointer<Tuple>{}));
+using type_bundles_t = decltype(type_bundles(as<Tuple*>{}));
 
-template <std::size_t N, class T>
+template <size_t N, class T>
 struct type_bundles_element;
 
-template <std::size_t N, class T, class ...Types>
+template <size_t N, class T, class ...Types>
 struct type_bundles_element<N, type_bundles<T, Types...>>: type_bundles_element<N - 1, type_bundles<Types...>> {};
 
 template <class T, class ...Types>
@@ -33,6 +32,6 @@ struct type_bundles_element<0, type_bundles<T, Types...>> {
     using type = T;
 };
 
-template <std::size_t N, class T> using type_bundles_element_t = typename type_bundles_element<N, T>::type;
+template <size_t N, class T> using type_bundles_element_t = typename type_bundles_element<N, T>::type;
 } /* nxwheels::concept_check::meta */
 #endif
