21c40ab7c78e31b822ac4e7d17e8db92a7948dd0
NobodyXu <johnxujiahao2000@gmail.com>
2018-06-20 17:39:19 +1000

    Add disjunction and else_if_t to control_flow.cc.

--- src/control_flow.hpp
+++ src/control_flow.hpp
@@ -17,5 +17,35 @@ struct _if<0, True, False> {
 };
 template <bool condition, class True, class False>
 using if_t = typename _if<condition, True, False>::type;
+
+template <class ...Ts> struct disjunction_impl;
+template <class T, class ...Ts>
+struct disjunction_impl<T, Ts...> {
+    using type = if_t<bool(T::value) || !bool(sizeof...(Ts)), T, typename disjunction_impl<Ts...>::type>;
+};
+template <>
+struct disjunction_impl<> {
+    using type = false_type;
+};
+template <class ...Ts> using disjunction_t = typename disjunction_impl<Ts...>::type;
+template <class ...Ts>
+struct disjunction: disjunction_t<Ts...> {};
+template <class ...Ts>
+constexpr const static inline bool disjunction_v = disjunction_t<Ts...>::value;
+/*!
+ * else_if_t is intended to make chaining if_t easier.
+ * But it requires a helper class to wrap the boolean value and the type.
+ */
+template <bool condition, class T>
+struct branch {
+    constexpr const static inline bool value = condition;
+    typedef T type;
+};
+template <class ...else_if_bundles>
+struct _else_if {
+    using type = typename disjunction_t<else_if_bundles...>::type;
+};
+template <class ...else_if_bundles>
+using else_if_t = typename _else_if<else_if_bundles...>::type;
 } /* nxwheels::concept_check */
 #endif
--- test/test_control_flow.cc
+++ test/test_control_flow.cc
@@ -1,7 +1,20 @@
 #include <type_traits>
 #include "../src/control_flow.hpp"
 using namespace nxwheels::concept_check;
+
+struct ft: std::false_type {};
+struct tt: std::true_type {};
 int main() {
+    // Test if_t.
     static_assert(std::is_same<if_t<0, int, char>, char>{}());
     static_assert(std::is_same<if_t<1, int, char>, int>{}());
+
+    // Test disjunction_t.
+    static_assert(std::is_same<disjunction_t<std::false_type, ft, tt>, tt>{}());
+    static_assert(std::is_same<disjunction_t<>, false_type>{}());
+
+    // Test else_if_t.
+    static_assert(std::is_same<else_if_t<branch<false, void>,
+                            branch<true, int>
+    >, int>{}());
 }
