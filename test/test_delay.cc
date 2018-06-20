92273e9b6f5c784fad0375da4b3be7015208ddc9
NobodyXu <johnxujiahao2000@gmail.com>
2018-06-20 23:45:36 +1000

    Add delay.hpp.

---
+++ test/test_delay.cc
@@ -0,0 +1,10 @@
+#include "../src/is_same.hpp"
+#include "../src/delay.hpp"
+using namespace nxwheels::concept_check;
+
+template <class T>
+using as_itself = T;
+int main() {
+    assert_same<evaluate_t<int>, int>();
+    assert_same<evaluate_t<delay<as_itself, int>>, int>();
+}
