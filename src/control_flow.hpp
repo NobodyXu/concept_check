ba3fa60f579a5ecf5197bc0d88084fa23df86f77
NobodyXu <johnxujiahao2000@gmail.com>
2018-06-21 17:37:18 +1000

    The code regarding disjunction_t and else_if_t is finisned and tested.

--- src/control_flow.hpp
+++ src/control_flow.hpp
@@ -1,27 +1,34 @@
 /*!
  * This file contains control flows for template meta programming.
- * Inorder to be consistent with ```if```, ```else``` and other control flow statement used in normal programming, but not conflict with C++ standard
+ * In order to be consistent with ```if```, ```else``` and other control flow statement used in normal programming, but not conflict with C++ standard
  * names, an underscore will be inserted before the original class name.
+ * All the template in this file supports lazy instantiation of the result type.
  */
 #ifndef __nobodyxu_concept_check_control_flow_HPP__
 # define __nobodyxu_concept_check_control_flow_HPP__
+
 # include "detector_core.hpp"
+# include "delay.hpp"
+# include "concepts/has_nested_type_type.hpp"
+
 namespace nxwheels::concept_check {
-template <bool condition, class True, class False>
+template <bool condition, class when_True, class when_False>
 struct _if {
-    typedef True  type;
+    typedef evaluate_t<when_True> type;
 };
-template <class True, class False>
-struct _if<0, True, False> {
-    typedef False type;
+template <class when_True, class when_False>
+struct _if<0, when_True, when_False> {
+    typedef evaluate_t<when_False> type;
 };
-template <bool condition, class True, class False>
-using if_t = typename _if<condition, True, False>::type;
+template <bool condition, class when_True, class when_False> using if_t = typename _if<condition, when_True, when_False>::type;
 
 template <class ...Ts> struct disjunction_impl;
 template <class T, class ...Ts>
 struct disjunction_impl<T, Ts...> {
-    using type = if_t<bool(T::value) || !bool(sizeof...(Ts)), T, typename disjunction_impl<Ts...>::type>;
+    using type = if_t<bool(T::value) || !bool(sizeof...(Ts)),
+                        T,
+                        delay<nested_type_type_t, delay<disjunction_impl, Ts...>>
+    >;
 };
 template <>
 struct disjunction_impl<> {
@@ -32,6 +39,7 @@ template <class ...Ts>
 struct disjunction: disjunction_t<Ts...> {};
 template <class ...Ts>
 constexpr const static inline bool disjunction_v = disjunction_t<Ts...>::value;
+
 /*!
  * else_if_t is intended to make chaining if_t easier.
  * But it requires a helper class to wrap the boolean value and the type.
--- test/test_control_flow.cc
+++ test/test_control_flow.cc
@@ -4,10 +4,19 @@ using namespace nxwheels::concept_check;
 
 struct ft: std::false_type {};
 struct tt: std::true_type {};
+
+template <class T>
+using as_origin = T;
 int main() {
     // Test if_t.
     static_assert(std::is_same<if_t<0, int, char>, char>{}());
     static_assert(std::is_same<if_t<1, int, char>, int>{}());
+
+    static_assert(std::is_same<if_t<0, delay<as_origin, int>, char>, char>{}());
+    static_assert(std::is_same<if_t<1, char, delay<as_origin, int>>, char>{}());
+
+    static_assert(std::is_same<if_t<0, delay<as_origin, int>, delay<as_origin, char>>, char>{}());
+    static_assert(std::is_same<if_t<1, delay<as_origin, char>, delay<as_origin, int>>, char>{}());
 
     // Test disjunction_t.
     static_assert(std::is_same<disjunction_t<std::false_type, ft, tt>, tt>{}());
