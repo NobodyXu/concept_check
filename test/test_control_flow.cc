e50d0ed1b164b01b5d01c6c70ad450ffe866a011
NobodyXu <johnxujiahao2000@gmail.com>
2018-06-21 17:46:16 +1000

    Add tests fo else_if_t:
     1. When no branch is true;
     2. When no parameter is given.

--- test/test_control_flow.cc
+++ test/test_control_flow.cc
@@ -1,30 +1,41 @@
-#include <type_traits>
+#include "../src/is_same.hpp"
 #include "../src/control_flow.hpp"
 using namespace nxwheels::concept_check;
 
-struct ft: std::false_type {};
-struct tt: std::true_type {};
+struct ft: false_type {};
+struct tt: true_type {};
 
 template <class T>
 using as_origin = T;
 int main() {
     // Test if_t.
-    static_assert(std::is_same<if_t<0, int, char>, char>{}());
-    static_assert(std::is_same<if_t<1, int, char>, int>{}());
+    assert_same<if_t<0, int, char>, char>();
+    assert_same<if_t<1, int, char>, int>();
 
-    static_assert(std::is_same<if_t<0, delay<as_origin, int>, char>, char>{}());
-    static_assert(std::is_same<if_t<1, char, delay<as_origin, int>>, char>{}());
+    assert_same<if_t<0, delay<as_origin, int>, char>, char>();
+    assert_same<if_t<1, char, delay<as_origin, int>>, char>();
 
-    static_assert(std::is_same<if_t<0, delay<as_origin, int>, delay<as_origin, char>>, char>{}());
-    static_assert(std::is_same<if_t<1, delay<as_origin, char>, delay<as_origin, int>>, char>{}());
+    assert_same<if_t<0, delay<as_origin, int>, delay<as_origin, char>>, char>();
+    assert_same<if_t<1, delay<as_origin, char>, delay<as_origin, int>>, char>();
 
     // Test disjunction_t.
-    static_assert(std::is_same<disjunction_t<std::false_type, ft, tt>, tt>{}());
-    static_assert(std::is_same<disjunction_t<>, false_type>{}());
-    static_assert(std::is_same<disjunction_t<ft>, ft>{}());
+    assert_same<disjunction_t<false_type, ft, tt>, tt>();
+    assert_same<disjunction_t<>, false_type>();
+    assert_same<disjunction_t<ft>, ft>();
 
     // Test else_if_t.
-    static_assert(std::is_same<else_if_t<branch<false, void>,
-                            branch<true, int>
-    >, int>{}());
+    assert_same<
+        else_if_t<
+            branch<false, void>,
+            branch<true, int>
+        >,
+    int>();
+    assert_same<
+        else_if_t<
+            branch<false, void>,
+            branch<false, void>,
+            branch<false, void>
+        >,
+    void>();
+    assert_same<else_if_t<>, false_type>();
 }
