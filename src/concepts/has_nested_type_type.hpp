b21f6a6b70cbf6cb37ce37e3cb0fb1279d004340
NobodyXu <johnxujiahao2000@gmail.com>
2018-06-21 16:10:34 +1000

    Add concepts/has_nested_type_type.hpp.

---
+++ src/concepts/has_nested_type_type.hpp
@@ -0,0 +1,12 @@
+#ifndef __nobodyxu_concept_check_has_nested_type_type_HPP__
+# define __nobodyxu_concept_check_has_nested_type_type_HPP__
+# include "../detector_core.hpp"
+namespace nxwheels::concept_check {
+template <class T> using nested_type_type_t = typename T::type;
+
+template <class T>
+constexpr const static inline bool has_nested_type_type_v = is_detected_v<nested_type_type_t, T>;
+template <class T> 
+using has_nested_type_type = is_detected<nested_type_type_t, T>;
+} /* nxwheels::concept_check */
+#endif
---
+++ test/concepts/test_has_nested_type_type.cc
@@ -0,0 +1,11 @@
+#include "../../src/is_same.hpp"
+#include "../../src/concepts/has_nested_type_type.hpp"
+using namespace nxwheels::concept_check;
+
+struct A {
+    typedef A type;
+};
+int main() {
+    assert_same<nested_type_type_t<A>, A>();
+    static_assert(has_nested_type_type_v<A>);
+}
