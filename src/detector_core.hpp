0e78f5d7e52289306f1ba8c09a9d155c1cb2f09c
NobodyXu <johnxujiahao2000@gmail.com>
2018-06-21 21:17:20 +1000

    Improve format.

--- src/detector_core.hpp
+++ src/detector_core.hpp
@@ -4,29 +4,37 @@
 # include "void_t.hpp"
 # include "nonsuch.hpp"
 # include "integral_constant.hpp"
+
 namespace nxwheels::concept_check::impl {
-template <class, template <class...> class op, class ...Ts>
+template <class Default, class always_void, template <class...> class op, class ...Ts>
 struct detector: false_type {
-    using result_type = nonsuch;
+    using result_type = Default;
 };
 
-template <template <class...> class op, class ...Ts>
-struct detector<void_t<op<Ts...>>, op, Ts...>: true_type {
+template <class Default, template <class...> class op, class ...Ts>
+struct detector<Default, void_t<op<Ts...>>, op, Ts...>: true_type {
     using result_type = op<Ts...>;
 };
 } /* nxwheels::concept_check::impl */
 
 namespace nxwheels::concept_check {
 template <template <class...> class op, class ...Ts>
-using detector_t = impl::detector<void, op, Ts...>;
+using detector_t = impl::detector<nonsuch, void, op, Ts...>;
 
 template <template <class...> class op, class ...Ts>
-using is_detected = impl::detector<void, op, Ts...>;
+using is_detected = impl::detector<nonsuch, void, op, Ts...>;
+
+template <template <class...> class op, class ...Ts>
+constexpr const static inline bool is_detected_v = detector_t<op, Ts...>::value;
 
 template <template <class...> class op, class ...Ts>
 using detected_t = typename detector_t<op, Ts...>::result_type;
 
-template <template <class...> class op, class ...Ts>
-constexpr const static inline bool is_detected_v = detector_t<op, Ts...>::value;
+template <class Default, template <class...> class op, class ...Ts>
+using detected_or_t = impl::detector<Default, void, op, Ts...>;
+template <class Default, template <class...> class op, class ...Ts>
+struct detected_or {
+    typedef detected_or_t<Default, op, Ts...> type;
+};
 } /* nxwheels::concept_check */
 #endif
