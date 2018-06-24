8a9a30caf461a0e672b88e52798f64b14e3a3715
NobodyXu <johnxujiahao2000@gmail.com>
2018-06-24 21:17:49 +1000

    Add decay_t.hpp.

---
+++ test/test_decay_t.cc
@@ -0,0 +1,36 @@
+#include "../src/decay_t.hpp"
+#include "../src/concepts/is_same.hpp"
+using namespace nxwheels::concept_check;
+
+int main() {
+    // Test decay_t against the 3 rules of decay_t.
+    // Rule 1
+    typedef int array[10];
+    typedef int array2[10][10];
+    assert_same<decay_t<array>, int*>();
+    assert_same<decay_t<array&>, int*>();
+    assert_same<decay_t<array&&>, int*>();
+    assert_same<decay_t<array2>, array*>();
+    assert_same<decay_t<array2&&>, array*>();
+    // Rule 2
+    assert_same<decay_t<int (void)>, int (*)(void)>();
+    assert_same<decay_t<int (&)(void)>, int (*)(void)>();
+
+    assert_same<decay_t<int() &>, int () &>();
+    assert_same<decay_t<int() &&>, int () &&>();
+    assert_same<decay_t<int() const>, int () const>();
+    assert_same<decay_t<int() const volatile &&>, int () const volatile &&>();
+
+    assert_same<decay_t<int () noexcept>, int (*)() noexcept>();
+
+    // Rule 3
+    assert_same<decay_t<int>, int>();
+    assert_same<decay_t<const int>, int>();
+    assert_same<decay_t<volatile int>, int>();
+    assert_same<decay_t<const volatile int>, int>();
+
+    assert_same<decay_t<int&>, int>();
+    assert_same<decay_t<int&&>, int>();
+
+    assert_same<decay_t<const volatile int &&>, int>();
+}
