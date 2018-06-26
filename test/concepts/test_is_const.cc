86c77d32f72075b467b480d33041541f7a1e58ff
NobodyXu <johnxujiahao2000@gmail.com>
2018-06-26 20:08:56 +1000

    Add concepts/is_const.hpp.

---
+++ test/concepts/test_is_const.cc
@@ -0,0 +1,9 @@
+#include "../../src/concepts/is_const.hpp"
+using namespace nxwheels::concept_check;
+
+int main() {
+    static_assert(!is_const_v<int>);
+
+    static_assert(is_const_v<const int>);
+    static_assert(is_const_v<const volatile int>);
+}
