b1469259bb6ff59f524dd36edde3708bc81495c6
NobodyXu <johnxujiahao2000@gmail.com>
2018-07-02 12:53:23 +1000

    Rename concepts/is_pointer.hpp as concepts/is_primitive_pointer.hpp.

---
+++ src/concepts/is_primitive_pointer.hpp
@@ -0,0 +1,11 @@
+#ifndef __nobodyxu_concept_check_concepts_is_primitive_pointer_HPP__
+# define __nobodyxu_concept_check_concepts_is_primitive_pointer_HPP__
+# include "../integral_constant.hpp"
+namespace nxwheels::concept_check {
+template <class T>          struct is_primitive_pointer:         false_type {};
+template <class T>          struct is_primitive_pointer<T*>:     true_type {};
+template <class P, class T> struct is_primitive_pointer<P T::*>: true_type {};
+
+template <class T> constexpr const static inline bool is_primitive_pointer_v = is_primitive_pointer<T>::value;
+} /* nxwheels::concept_check */
+#endif
---
+++ src/concepts/is_primitive_v.hpp
@@ -0,0 +1,8 @@
+#ifndef __nodbodyxu_concept_check_concepts_is_primitive_v_HPP__
+# define __nodbodyxu_concept_check_concepts_is_primitive_v_HPP__
+# include "is_bool_v.hpp"
+# include "is_void_v.hpp"
+namespace nxwheels::concept_check {
+template <class T> constexpr const static inline bool is_primitive_v = ;
+} /* nxwheels::concept_check */
+#endif
