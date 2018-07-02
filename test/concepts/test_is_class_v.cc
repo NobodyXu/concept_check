6b89471042f6b371dfdebd83fd14551f645d06c0
NobodyXu <johnxujiahao2000@gmail.com>
2018-07-02 15:20:34 +1000

    Add concepts/is_class_v.hpp.

---
+++ test/concepts/test_is_class_v.cc
@@ -0,0 +1,20 @@
+#include "../../src/concepts/is_class_v.hpp"
+using namespace nxwheels::concept_check;
+
+enum unscoped_e {};
+enum class scoped_e {};
+struct S {};
+struct incomplete_class;
+union U {};
+
+int main() {
+    static_assert(!is_class_v<int>);
+    static_assert(!is_class_v<int*>);
+    static_assert(!is_class_v<void>);
+    static_assert(!is_class_v<unscoped_e>);
+    static_assert(!is_class_v<scoped_e>);
+    static_assert(!is_class_v<U>);
+
+    static_assert(is_class_v<S>);
+    static_assert(is_class_v<incomplete_class>);
+}
