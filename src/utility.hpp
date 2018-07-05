2fdbdce6476aca7cbbde13848e6b6a896ecfc93e
NobodyXu <johnxujiahao2000@gmail.com>
2018-07-05 17:18:00 +1000

    Simplify utility.hpp as this repository should only contain functions defined for type_traits.

--- src/utility.hpp
+++ src/utility.hpp
@@ -1,8 +1,6 @@
 #ifndef __nobodyxu_concept_check_declval_HPP__
 # define __nobodyxu_concept_check_declval_HPP__
 # include "add_reference.hpp"
-# include "add_cv_t.hpp"
-# include "remove_reference.hpp"
 namespace nxwheels::concept_check {
 template <class T>
 using as = T;
@@ -14,24 +12,5 @@ template <class T>
 auto declval() noexcept -> add_rvalue_reference_t<T>;
 template <class T>
 auto decl_as() noexcept -> T;
-
-template <class T>
-constexpr T&& forward(remove_reference_t<T> &obj) noexcept {
-    return as<T&&>(obj);
-}
-
-template <class T>
-constexpr decltype(auto) move(T &&t) noexcept {
-    return add_rvalue_reference_t<T>(t);
-}
-
-template <class T>
-constexpr decltype(auto) as_const(T &t) {
-    return as<add_const_t<T>&>(t);
-}
-template <class T>
-constexpr decltype(auto) as_const(T &&t) {
-    return as<add_const_t<T>&&>(t);
-}
 } /* nxwheels::concept_check */
 #endif
