f62c76596a0b9ae4ff188f32aa805d7b8a57ecf3
NobodyXu <johnxujiahao2000@gmail.com>
2018-07-05 16:57:40 +1000

    Add is_trivially_direct_constructible.

---
+++ test/concepts/type_properties/operations/test_is_trivially_constructible_v.cc
@@ -0,0 +1,73 @@
+#include <initializer_list>
+#include "../../../../src/concepts/type_properties/operations/is_trivially_constructible.hpp"
+using namespace nxwheels::concept_check;
+
+struct A {
+    A(int) {}
+    A(const A&) = default;
+};
+struct B {
+    B() = default;
+    B(int) {}
+    ~B() = delete;
+};
+struct C {
+    C(int) noexcept {}
+};
+struct Aggregate {
+    int i;
+    char c;
+};
+struct nontrivially_default_constructible_aggregate {
+    int i = 2222;
+    char c = 'd';
+};
+struct wrapper_over_aggregate {
+    Aggregate a;
+    wrapper_over_aggregate(int, char);
+};
+struct initializer_list_constructible {
+    initializer_list_constructible(std::initializer_list<int> il) {}
+};
+struct nothrow_initializer_list_constructible {
+    nothrow_initializer_list_constructible(std::initializer_list<int> il) noexcept {}
+};
+
+enum       unscoped_enum_int: int {};
+enum class scoped_enum_int:   int {};
+
+int main() {
+    // Test is_trivially_constructible_v.
+    static_assert(!is_trivially_default_constructible_v<A>);
+
+    static_assert(is_trivially_default_constructible_v<int>);
+    static_assert(is_trivially_default_constructible_v<Aggregate>);
+
+    // Test is_trivially_copy_constructible_v.
+    static_assert(is_trivially_copy_constructible_v<int>);
+    static_assert(is_trivially_copy_constructible_v<A>);
+    static_assert(is_trivially_copy_constructible_v<Aggregate>);
+
+    // Test is_trivially_direct_constructible_v.
+    static_assert(is_trivially_direct_constructible_v<int>);
+    static_assert(is_trivially_direct_constructible_v<int, int>);
+    static_assert(is_trivially_direct_constructible_v<A, A>);
+    static_assert(is_trivially_direct_constructible_v<A, const A&>);
+    static_assert(is_trivially_direct_constructible_v<A, A&&>);
+    static_assert(is_trivially_direct_constructible_v<B>);
+
+    static_assert(is_trivially_direct_constructible_v<Aggregate>);
+    static_assert(is_trivially_direct_constructible_v<Aggregate, Aggregate>);
+    static_assert(is_trivially_direct_constructible_v<Aggregate, const Aggregate&>);
+    static_assert(is_trivially_direct_constructible_v<Aggregate, Aggregate&&>);
+    static_assert(!is_trivially_direct_constructible_v<nontrivially_default_constructible_aggregate>);
+
+    static_assert(is_trivially_direct_constructible_v<unscoped_enum_int>);
+    static_assert(is_trivially_direct_constructible_v<scoped_enum_int>);
+
+    static_assert(!is_trivially_direct_constructible_v<std::initializer_list<int>>);
+    static_assert(is_trivially_direct_constructible_v<std::initializer_list<int>, std::initializer_list<int>>);
+    static_assert(is_trivially_direct_constructible_v<std::initializer_list<int>, const std::initializer_list<int>&>);
+    static_assert(is_trivially_direct_constructible_v<std::initializer_list<int>, std::initializer_list<int>&&>);
+    static_assert(!is_trivially_direct_constructible_v<std::initializer_list<int>, int>);
+}
