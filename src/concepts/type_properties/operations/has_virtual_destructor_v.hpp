b12e13e5dad95e512ef3d036d31d0982ee560b97
NobodyXu <johnxujiahao2000@gmail.com>
2018-07-05 21:26:09 +1000

    Add concepts/type_properties/operations/has_virtual_destructor_v.hpp. Because it relies on compiler builtin, so it's not tested.

---
+++ src/concepts/type_properties/operations/has_virtual_destructor_v.hpp
@@ -0,0 +1,6 @@
+#ifndef __nobodyxu_concept_check_concepts_type_properties_operations_has_virtual_destructor_v_HPP__
+# define __nobodyxu_concept_check_concepts_type_properties_operations_has_virtual_destructor_v_HPP__
+namespace nxwheels::concept_check {
+template <class T> constexpr const static inline bool has_virtual_destuctor_v = __has_virtual_destructor(T);
+} /* nxwheels::concept_check */
+#endif
