c83bf5f356819cb3f8029362563dcad7684edc90
NobodyXu <johnxujiahao2000@gmail.com>
2018-07-05 21:22:42 +1000

    Add concepts/type_properties/operations/is_trivially_destructible_v.hpp.

---
+++ src/concepts/type_properties/operations/is_trivially_destructible_v.hpp
@@ -0,0 +1,6 @@
+#ifndef __nobodyxu_concept_check_concepts_type_properties_operations_is_trivially_destructible_v_HPP__
+# define __nobodyxu_concept_check_concepts_type_properties_operations_is_trivially_destructible_v_HPP__
+namespace nxwheels::concept_check {
+template <class T> constexpr const static inline bool is_trivially_destructible_v = __has_trivial_destructor(T);
+} /* nxwheels::concept_check */
+#endif
