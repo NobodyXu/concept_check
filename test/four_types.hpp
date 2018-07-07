07a9e6b2344931876e7297feda2825b7b4814512
NobodyXu <johnxujiahao2000@gmail.com>
2018-07-07 13:43:15 +1000

    Add test/four_types.hpp to make testing easier.

---
+++ test/four_types.hpp
@@ -0,0 +1,7 @@
+#ifndef __nobodyxu_concpet_check_test_four_types_HPP__
+# define __nobodyxu_concpet_check_test_four_types_HPP__
+struct S {};
+union U {};
+enum unscoped_e {};
+enum class scoped_e {};
+#endif
