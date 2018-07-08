610c5ffed84ca91bdb940089eaa5330b0d9b2159
NobodyXu <johnxujiahao2000@gmail.com>
2018-07-08 13:51:54 +1000

    Add concepts/type_properties/operations/is_dereferenceable_v.hpp.

---
+++ test/concepts/type_properties/operations/test_is_dereferenceable_v.cc
@@ -0,0 +1,37 @@
+#include "../../../../src/concepts/type_properties/operations/is_dereferenceable_v.hpp"
+using namespace nxwheels::concept_check;
+
+struct S {};
+struct S0 {
+    int *i;
+    auto operator * () const { return *i; }
+};
+struct S1 {
+    int *i;
+    auto operator * () const noexcept { return *i; }
+};
+
+int main() {
+    // Test is_dereferenceable_v.
+    {
+
+        static_assert(!is_dereferenceable_v<int>);
+
+        static_assert(is_dereferenceable_v<int*>);
+        static_assert(is_dereferenceable_v<S0>);
+        static_assert(is_dereferenceable_v<S1>);
+
+    }
+
+    // Test is_nothrow_dereferenceable_v.
+    {
+
+        static_assert(!is_nothrow_dereferenceable_v<int>);
+        static_assert(!is_nothrow_dereferenceable_v<S0>);
+
+        static_assert(is_nothrow_dereferenceable_v<int*>);
+        static_assert(is_nothrow_dereferenceable_v<S1>);
+
+    }
+
+}
