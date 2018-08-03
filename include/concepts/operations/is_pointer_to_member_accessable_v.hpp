b0f97bcebff8e81fa408f6ed5592a4f2ac642a90
NobodyXu <johnxujiahao2000@gmail.com>
2018-08-03 23:03:54 +1000

    1. Delete has_virtual_destructor_v.hpp, is_destructible_v.hpp, is_trivially_constructible.hpp and is_trivially_destructible_v.hpp under concepts/operations because there is already one in c++11 and is well supported and my version doesn't add anything new.  2. Remove excessive comment in concepts/operations/is_pointer_to_member_accessable_v.hpp because it affects readability.  3. In

--- include/concepts/operations/is_pointer_to_member_accessable_v.hpp
+++ include/concepts/operations/is_pointer_to_member_accessable_v.hpp
@@ -9,30 +9,13 @@
 # include "../is_member_pointer.hpp"
 
 namespace nxwheels {
-// Beg of impl of is_pointer_to_member_accessable_v.
 template <class T, class P> using pointer_to_data_member_accessed_t = decltype( declval<T>()->*declval<P>() );
-/*!
- * Test whether declval<T>()->*declval<P>() is valid.
- */
 template <class T, class P> constexpr const static inline bool is_pointer_to_data_member_accessable_v = is_detected_v<pointer_to_data_member_accessed_t, T, P>;
-// End of impl of is_pointer_to_member_accessable_v.
 
-// Beg of impl of is_pointer_to_function_member_accessable_v.
 template <class T, class P, class ...Args> using pointer_to_function_member_accessed_t = decltype( (declval<T>()->*declval<P>())(declval<Args>()...) );
-/*!
- * Test whether (declval<T>()->*declval<P>())(declval<Args>()...) is valid.
- * This is the implementation for _is_pointer_to_function_member_accessable_v.
- */
 template <class T, class P, class ...Args>
 constexpr const static inline bool __is_pointer_to_function_member_accessable_v = is_detected_v<pointer_to_function_member_accessed_t, T, P, Args...>;
-/*!
- * Class wrapper for _is_pointer_to_function_member_accessable_v so that it can be passed as a class template.
- */
 template <class T, class P, class ...Args> struct __is_pointer_to_function_member_accessable: bool_constant<__is_pointer_to_function_member_accessable_v<T, P, Args...>> {};
-/*!
- * Implementation of is_pointer_to_function_member_accessable_v.
- * _is_pointer_to_function_member_accessable_v assumes template parameter P is a function pointer type.
- */
 template <class T, class P>
 constexpr const static inline bool _is_pointer_to_function_member_accessable_v = apply_function_args_to_t<pointed_to_by_member_pointer_t<P>,
                                                                                                           PARTIAL_APPLY_T(__is_pointer_to_function_member_accessable, T, P)>::value;
@@ -43,26 +26,19 @@ constexpr const static inline bool is_pointer_to_function_member_accessable_v = 
     else
         return false;
 }();
-// End of impl of is_pointer_to_function_member_accessable_v.
 
-// Beg of impl of _is_nothrow_pointer_to_data_member_accessable_v and _is_nothrow_pointer_to_function_member_accessable_v.
 template <class T, class P> constexpr const static inline bool _is_nothrow_pointer_to_data_member_accessable_v = noexcept( declval<T>()->*declval<P>() );
 template <class T, class P, class ...Args>
 constexpr const static inline bool __is_nothrow_pointer_to_function_member_accessable_v = noexcept( (declval<T>()->*declval<P>())(declval<Args>()...) );
 template <class T, class P, class ...Args>
 struct __is_nothrow_pointer_to_function_member_accessable: bool_constant<__is_nothrow_pointer_to_function_member_accessable_v<T, P, Args...>> {};
-// End of impl of _is_nothrow_pointer_to_data_member_accessable_v and _is_nothrow_pointer_to_function_member_accessable_v.
 
-// Beg of impl of is_nothrow_pointer_to_data_member_accessable_v and is_nothrow_pointer_to_function_member_accessable_v.
 template <class T, class P> constexpr const static inline bool is_nothrow_pointer_to_data_member_accessable_v = []{
     if constexpr(is_pointer_to_data_member_accessable_v<T, P>)
         return _is_nothrow_pointer_to_data_member_accessable_v<T, P>;
     else
         return false;
 }();
-/*!
- * _is_nothrow_pointer_to_function_member_accessable_v assumes is_member_function_pointer_v<P> == true.
- */
 template <class T, class P> constexpr const static inline bool _is_nothrow_pointer_to_function_member_accessable_v = []{
     if constexpr(_is_pointer_to_function_member_accessable_v<T, P>)
         return apply_function_args_to_t<pointed_to_by_member_pointer_t<P>, PARTIAL_APPLY_T(__is_nothrow_pointer_to_function_member_accessable, T, P)>::value;
@@ -75,9 +51,7 @@ template <class T, class P> constexpr const static inline bool is_nothrow_pointe
     else
         return false;
 }();
-// End of impl of is_nothrow_pointer_to_data_member_accessable_v and is_nothrow_pointer_to_function_member_accessable_v.
 
-// Beg of impl of is_pointer_to_member_accessable_v and is_nothrow_pointer_to_member_accessable_v.
 template <class T, class P> constexpr const static inline bool is_pointer_to_member_accessable_v = []{
     if constexpr(is_member_function_pointer_v<P>)
         return _is_pointer_to_function_member_accessable_v<T, P>;
@@ -90,6 +64,5 @@ template <class T, class P> constexpr const static inline bool is_nothrow_pointe
     else
         return is_nothrow_pointer_to_data_member_accessable_v<T, P>;
 }();
-// End of impl of is_pointer_to_member_accessable_v and is_nothrow_pointer_to_member_accessable_v.
 } /* nxwheels */
 #endif
