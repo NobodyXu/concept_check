c8ef40f313aa6cc2789e25e05e32c2ed3ef8c8e4
NobodyXu <johnxujiahao2000@gmail.com>
2018-08-14 23:33:49 +1000

    Add is_(nothrow_)(exact/implicitly_convertible)_dereferenceable_v.

--- include/concepts/operations/is_dereferenceable_v.hpp
+++ include/concepts/operations/is_dereferenceable_v.hpp
@@ -1,10 +1,24 @@
 #ifndef __nobodyxu_concept_check_concepts_operations_is_dereferenceable_v_HPP__
 # define __nobodyxu_concept_check_concepts_operations_is_dereferenceable_v_HPP__
 
+# include "is_convertible_v.hpp"
 # include "def_convenient_macros.hpp"
 
 namespace nxwheels {
 DEF_UN_CHECK(dereference, *);
+
+template <class T> using dereferenced_or_nonsuch_t = detected_or_t<nonsuch, dereferenceed_ret_t, T>;
+
+# define DEF_CONCEPT template <class T, class Ret> constexpr const static inline bool
+
+DEF_CONCEPT is_exact_dereferenceable_v         = std::is_same<dereferenced_or_nonsuch_t<T>, Ret>{}();
+DEF_CONCEPT is_nothrow_exact_dereferenceable_v = is_nothrow_dereferenceable_v<T> && is_exact_dereferenceable_v<T, Ret>;
+
+DEF_CONCEPT is_implicitly_convertible_dereferenceable_v         = is_implicitly_convertible_v<dereferenced_or_nonsuch_t<T>, Ret>;
+DEF_CONCEPT is_nothrow_implicitly_convertible_dereferenceable_v = is_nothrow_dereferenceable_v<T> && is_implicitly_convertible_dereferenceable_v<T, Ret>;
+
+# undef DEF_CONCEPT
+
 DEF_UN_CHECK_T(dereference);
 } /* nxwheels */
 
