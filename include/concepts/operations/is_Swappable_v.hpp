b247997a9e2fb5adb1e293b2813e919e1bd5f722
NobodyXu <johnxujiahao2000@gmail.com>
2018-08-15 21:59:33 +1000

    Add is_Swappable_v.hpp that automatically includes std::swap and using directive for it.

---
+++ include/concepts/operations/is_Swappable_v.hpp
@@ -0,0 +1,10 @@
+#ifndef __nobodyxu_concept_check_concepts_operations_is_Swappable_v_HPP__
+# define __nobodyxu_concept_check_concepts_operations_is_Swappable_v_HPP__
+
+# include <algorithm>
+# include <utility>
+using std::swap;
+
+# include "is_swappable_v.hpp"
+
+#endif
