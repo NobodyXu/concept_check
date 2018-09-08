697dabdd4a7db714b9ba4d0046f8dc5ebadf2d26
NobodyXu <johnxujiahao2000@gmail.com>
2018-09-08 21:16:25 +1000

    Add concepts/is_same.hpp.

---
+++ test/concepts/test_is_same.cc
@@ -0,0 +1,17 @@
+#include "../../include/concepts/is_same.hpp"
+using namespace nxwheels;
+
+int main() {
+    assert_not_same_type<>();
+    assert_not_same_type<int>();
+    assert_same_type<int, int>();
+    assert_same_type<int, int, int>();//3
+    assert_same_type<int, int, int, int>();//4
+    assert_same_type<int, int, int, int, int>();//5
+    assert_same_type<int, int, int, int, int, int>();//6
+    assert_same_type<int, int, int, int, int, int, int>();//7
+    assert_same_type<int, int, int, int, int, int, int, int>();//8
+    assert_same_type<int, int, int, int, int, int, int, int, int>();//9
+    assert_same_type<int, int, int, int, int, int, int, int, int, int>();//10
+    assert_same_type<int, int, int, int, int, int, int, int, int, int, int>();//11
+}
