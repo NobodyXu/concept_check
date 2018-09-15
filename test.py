e9d6b00754ee850b30466b9848fdb1593e7eae9c
NobodyXu <johnxujiahao2000@gmail.com>
2018-09-15 16:14:39 +1000

    Substituate Makefile with test.py.

---
+++ test.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+import os
+import glob
+import subprocess
+
+def invoke(*args):
+    print(*args)
+    subprocess.run(*args, check = True)
+
+repo = os.path.dirname(os.path.realpath(__file__))
+test = repo + "/test/"
+
+for relative_path in ["/", "/concepts/operations/", "/concepts/member/", "/concepts/"]:
+    # Since the test in this repository just do some compile-time checking, so it is only compiled to *.o.
+    # By doing this, calls to the compiler can be chained.
+    invoke([os.environ["CXX"], "-std=c++17", "-c"] + glob.glob(test + relative_path + "*.cc"))
