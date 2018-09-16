e3010341d64fbbcdf26145b50b45e6c3f6772354
NobodyXu <johnxujiahao2000@gmail.com>
2018-09-16 12:34:20 +1000

    Remove excessive test cases.

--- test/concepts/member/test_has_member_type_iterator_category_v.cc
+++ test/concepts/member/test_has_member_type_iterator_category_v.cc
@@ -8,11 +8,10 @@ struct A {
 
 int main() {
     static_assert(!has_member_type_iterator_category_v<void>);
+
     static_assert(!has_member_type_iterator_category_v<int>);
+
     static_assert(!has_member_type_iterator_category_v<S>);
-    static_assert(!has_member_type_iterator_category_v<U>);
-    static_assert(!has_member_type_iterator_category_v<unscoped_e>);
-    static_assert(!has_member_type_iterator_category_v<scoped_e>);
 
     static_assert(has_member_type_iterator_category_v<A>);
 }
