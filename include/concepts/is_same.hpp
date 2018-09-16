6b165ca8f82ee87410d761eb70c0fb8a170d2c7e
NobodyXu <johnxujiahao2000@gmail.com>
2018-09-16 18:02:03 +1000

    1. Fix is_same.  2. Remove excessive comment.  3. Remove excessive optimization on is_same.  4. Add test cases for which assert_not_same should not fail.  5. Remove excessive tests.

--- include/concepts/is_same.hpp
+++ include/concepts/is_same.hpp
@@ -2,25 +2,28 @@
 # define __nobodyxu_concept_check_concepts_is_same_HPP__
 
 # include "../bool_constant.hpp"
+# include "../enable_if.hpp"
 
 namespace nxwheels {
 /*!
  * is_same compares whether all types in Args is the same. If sizeof...(Args) < 2, the value is false.
  */
-template <class ...Args> struct is_same: false_type {};
+template <class always_void, class ...Args> struct is_same_impl: false_type {};
 
 // The loop.
-template <class T1, class T2, class ...Args>
-struct is_same<T1, T2, Args...>:             bool_constant<is_same<T1, T2>::value             && is_same<T2, Args...>::value> {};
 template <class T1, class T2, class T3, class ...Args>
-struct is_same<T1, T2, T3, Args...>:         bool_constant<is_same<T1, T2, T3>::value         && is_same<T3, Args...>::value> {};
+struct is_same_impl<enable_if_t<sizeof...(Args) != 0>, T1, T2, T3, Args...>: bool_constant<is_same_impl<void, T1, T2, T3>::value && is_same_impl<void, T3, Args...>::value> {};
 
 // The end of loop.
-template <class T> struct is_same<T, T>:          true_type {};
-template <class T> struct is_same<T, T, T>:       true_type {};
+template <class T> struct is_same_impl<void, T, T>:    true_type {};
+template <class T> struct is_same_impl<void, T, T, T>: true_type {};
+
+template <class ...Args> struct is_same: is_same_impl<void, Args...> {};
 
 template <class ...Args> constexpr const static inline bool is_same_v = is_same<Args...>::value;
+
 template <class ...Args> constexpr void assert_same_type() noexcept { static_assert(is_same_v<Args...>, "They are not the same type!"); }
+
 template <class ...Args> constexpr void assert_not_same_type() noexcept { static_assert(!is_same_v<Args...>, "They are the same type!"); }
 } /* nxwheels */
 #endif
--- test/concepts/test_is_same.cc
+++ test/concepts/test_is_same.cc
@@ -3,16 +3,15 @@ using namespace nxwheels;
 
 int main() {
     assert_not_same_type<>();
+
     assert_not_same_type<int>();
+
     assert_not_same_type<int, char>();
     assert_same_type<int, int>();
+
+    assert_not_same_type<int, char, char>();
     assert_same_type<int, int, int>();//3
+
+    assert_not_same_type<int, char, char, char>();
     assert_same_type<int, int, int, int>();//4
-    assert_same_type<int, int, int, int, int>();//5
-    assert_same_type<int, int, int, int, int, int>();//6
-    assert_same_type<int, int, int, int, int, int, int>();//7
-    assert_same_type<int, int, int, int, int, int, int, int>();//8
-    assert_same_type<int, int, int, int, int, int, int, int, int>();//9
-    assert_same_type<int, int, int, int, int, int, int, int, int, int>();//10
-    assert_same_type<int, int, int, int, int, int, int, int, int, int, int>();//11
 }
