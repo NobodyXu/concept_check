263e1baaf46c161ef57d6f9aa2ac2612a15c6ca0
NobodyXu <johnxujiahao2000@gmail.com>
2018-09-16 19:04:53 +1000

    Add pointed_to_by_pointer_t.

--- include/concepts/is_builtin_pointer.hpp
+++ include/concepts/is_builtin_pointer.hpp
@@ -1,11 +1,23 @@
 #ifndef __nobodyxu_concept_check_concepts_is_builtin_pointer_HPP__
 # define __nobodyxu_concept_check_concepts_is_builtin_pointer_HPP__
 
+# include <type_traits>
+
 # include "is_builtin_nonmember_pointer.hpp"
 # include "is_member_pointer.hpp"
 
 namespace nxwheels {
 template <class T>
 constexpr const static inline bool is_builtin_pointer_v = is_builtin_nonmember_pointer_v<T> || is_member_pointer_v<T>;
+
+template <class T>
+struct pointed_to_by_pointer {};
+template <class T>
+struct pointed_to_by_pointer<T*>     { using type = T; };
+template <class P, class T>
+struct pointed_to_by_pointer<P T::*> { using type = P; };
+
+template <class T>
+using pointed_to_by_pointer_t = typename pointed_to_by_pointer<T>::type;
 } /* nxwheels */
 #endif
--- test/concepts/test_is_builtin_pointer.cc
+++ test/concepts/test_is_builtin_pointer.cc
@@ -1,15 +1,20 @@
 #include "../../include/concepts/is_builtin_pointer.hpp"
+#include "../../include/concepts/is_same.hpp"
 using namespace nxwheels;
 
 struct A {
     int i;
 };
 
-int main() {;
+int main() {
+    // Test is_builtin_pointer_v.
     static_assert(!is_builtin_pointer_v<int>);
 
     static_assert(is_builtin_pointer_v<int A::*>);
     static_assert(is_builtin_pointer_v<int*>);
     static_assert(is_builtin_pointer_v<void*>);
     static_assert(is_builtin_pointer_v<void**>);
+
+    // Test pointed_to_by_pointer_t.
+    static_assert(is_same_v<pointed_to_by_pointer_t<int A::*>, pointed_to_by_pointer_t<int*>, int>);
 }
