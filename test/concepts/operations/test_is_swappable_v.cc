83a8e4503e272c0464a7214d07a093cebe20e837
NobodyXu <johnxujiahao2000@gmail.com>
2018-09-16 13:03:11 +1000

    Remove excessive test cases.

--- test/concepts/operations/test_is_swappable_v.cc
+++ test/concepts/operations/test_is_swappable_v.cc
@@ -1,22 +1,14 @@
 #include <utility>
 using std::swap;
 
+#include "../../../include/concepts/operations/is_swappable_v.hpp"
+using namespace nxwheels;
+
 #include "../../four_types.hpp"
 struct S2 {};
 
 void swap(S&&, S&&) {}
 void swap(S2&&, S2&&) noexcept {}
-
-void swap(S&&, U&&) {}
-void swap(U&&, S&&) {}
-
-void swap(U&, S&&) noexcept {}
-void swap(S&&, U&) noexcept {}
-
-#include "../../../include/concepts/operations/is_swappable_v.hpp"
-using namespace nxwheels;
-template <class T> constexpr const static inline bool Is_swappable_v = is_swappable_with_v<T, T>;
-template <class T> constexpr const static inline bool Is_nothrow_swappable_v = is_nothrow_swappable_with_v<T, T>;
 
 int main() {
     // Test is_swappable_v.
@@ -57,46 +49,4 @@ int main() {
         static_assert(is_nothrow_swappable_v<S2>);
     }
 
-    // Test is_swappable_with_v.
-    {
-        static_assert(!Is_swappable_v<void>);
-        static_assert(!Is_swappable_v<void () const volatile && noexcept>);
-
-        static_assert(Is_swappable_v<int&>);
-        static_assert(Is_swappable_v<int (&)[22]>);
-        static_assert(Is_swappable_v<int*&>);
-        static_assert(Is_swappable_v<void (*&)() noexcept>);
-
-        static_assert(Is_swappable_v<S&>);
-        static_assert(Is_swappable_v<U&>);
-        static_assert(Is_swappable_v<unscoped_e&>);
-        static_assert(Is_swappable_v<scoped_e&>);
-
-        static_assert(Is_swappable_v<S>);
-        static_assert(Is_swappable_v<S2>);
-
-        static_assert(is_swappable_with_v<S, U>);
-        static_assert(is_swappable_with_v<S, U&>);
-    }
-
-    // Test is_nothrow_swappable_with_v.
-    {
-        static_assert(!Is_nothrow_swappable_v<void>);
-        static_assert(!Is_nothrow_swappable_v<void () const volatile && noexcept>);
-        static_assert(!Is_nothrow_swappable_v<S>);
-        static_assert(!is_nothrow_swappable_with_v<S, U>);
-
-        static_assert(Is_nothrow_swappable_v<int&>);
-        static_assert(Is_nothrow_swappable_v<int (&)[22]>);
-        static_assert(Is_nothrow_swappable_v<int*&>);
-        static_assert(Is_nothrow_swappable_v<void (*&)() noexcept>);
-
-        static_assert(Is_nothrow_swappable_v<S&>);
-        static_assert(Is_nothrow_swappable_v<U&>);
-        static_assert(Is_nothrow_swappable_v<unscoped_e&>);
-        static_assert(Is_nothrow_swappable_v<scoped_e&>);
-
-        static_assert(Is_nothrow_swappable_v<S2>);
-        static_assert(is_nothrow_swappable_with_v<S, U&>);
-    }
 }
