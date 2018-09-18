fd5eac32669d00dbce86df762de4617932942a3b
NobodyXu <johnxujiahao2000@gmail.com>
2018-09-19 01:01:41 +1000

    Use CONCEPT_T in /include/.

--- include/bool_constant.hpp
+++ include/bool_constant.hpp
@@ -1,10 +1,15 @@
 #ifndef __nobodyxu_concept_check_bool_constant_HPP__
 # define __nobodyxu_concept_check_bool_constant_HPP__
 
+# include "def_convenient_macros.hpp"
+
 namespace nxwheels {
-template <bool _value> struct bool_constant { constexpr const inline static bool value = _value; };
+template <bool _value> struct bool_constant { CONCEPT_T value = _value; };
 
 using true_type = bool_constant<true>;
 using false_type = bool_constant<false>;
 } /* nxwheels */
+
+# include "undef_convenient_macros.hpp"
+
 #endif
--- include/detector_v.hpp
+++ include/detector_v.hpp
@@ -4,8 +4,13 @@
 # include "detector_core.hpp"
 # include "concepts/operations/is_convertible_v.hpp"
 
+# include "def_convenient_macros.hpp"
+
 namespace nxwheels {
 template <class To, template <class...> class op, class ...Ts>
-constexpr const static inline bool is_detected_implicitly_convertible_v = is_implicitly_convertible_v<detected_t<op, Ts...>, To>;
+CONCEPT_T is_detected_implicitly_convertible_v = is_implicitly_convertible_v<detected_t<op, Ts...>, To>;
 } /* nxwheels */
+
+# include "undef_convenient_macros.hpp"
+
 #endif
