73a757d9972700b97c0d3453a5ee68b2d28e3a31
NobodyXu <johnxujiahao2000@gmail.com>
2018-09-18 18:57:49 +1000

    Revert "Add CONCEPT_T to  include/def_convenient_macros.hpp."
    This reverts commit 09635655001fe7f397a0bf7f11b51a15ec5d3bf2.

--- include/def_convenient_macros.hpp
+++
@@ -1,7 +0,0 @@
-#ifndef   CONCEPT_T
-# if   __cplusplus == 201703L
-#  define CONCEPT_T constexpr const static inline
-# elif __cplusplus == 201402L
-#  define CONCEPT_T constexpr const static
-# endif
-#endif
--- include/undef_convenient_macros.hpp
+++
@@ -1,3 +0,0 @@
-#ifdef  CONCEPT_T
-# undef CONCEPT_T
-#endif
