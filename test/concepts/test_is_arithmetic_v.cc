2612a6443ee207d0ac731f30041b71fb7fea5324
NobodyXu <johnxujiahao2000@gmail.com>
2018-10-11 18:15:54 +1100

    Use is_swappable_v instead of is_Swappable_v.

--- test/concepts/test_is_arithmetic_v.cc
+++ test/concepts/test_is_arithmetic_v.cc
@@ -1,6 +1,9 @@
 #include "../../include/concepts/is_arithmetic_v.hpp"
 using namespace nxwheels;
 
+#include <utility>
+using std::swap;
+
 int main() {
     static_assert(is_arithmetic_v<char>);
     static_assert(is_arithmetic_v<int>);
