d4fd9668c5ba325a4b8a6c11ee9ad892a4e80c74
NobodyXu <johnxujiahao2000@gmail.com>
2018-10-13 12:20:52 +1100

    Add more test cases.

--- test/concepts/test_is_iterator_v.cc
+++ test/concepts/test_is_iterator_v.cc
@@ -9,30 +9,49 @@
 using namespace nxwheels;
 
 int main() {
+    using InputIterator_t  = std::istream_iterator<int>;
+    using OutputIterator_t = std::back_insert_iterator<std::vector<int>>;
+
+    using const_ForwardIterator_t   = decltype(std::forward_list<int>().cbegin());
+    using mutable_ForwardIterator_t = decltype(std::forward_list<int>().begin());
+
+    using const_BidirectionalIterator_t   = decltype(std::list<int>().cbegin());
+    using mutable_BidirectionalIterator_t = decltype(std::list<int>().begin());
+
     static_assert(!is_Iterator_v<int>);
-    static_assert(!is_RandomAccessIterator_v<int>);
 
     // Input Iterator
-    static_assert(is_InputIterator_v<std::istream_iterator<int>>);
+    static_assert(is_InputIterator_v<InputIterator_t>);
 
     // Output Iterator
-    static_assert(is_Iterator_v<std::back_insert_iterator<std::vector<int>>>);
+    static_assert(is_Iterator_v<OutputIterator_t>);
 
     // Forward Iterator
-    static_assert(is_mutable_ForwardIterator_v<decltype(std::forward_list<int>().begin())>);
-    static_assert(is_const_ForwardIterator_v<decltype(std::forward_list<int>().cbegin())>);
+    {
+        static_assert(!is_ForwardIterator_v<InputIterator_t>);
+        static_assert(!is_ForwardIterator_v<OutputIterator_t>);
+
+        static_assert(is_mutable_ForwardIterator_v<mutable_ForwardIterator_t>);
+        static_assert(is_const_ForwardIterator_v<const_ForwardIterator_t>);
+    }
 
     // Bidirectional Iterator
-    static_assert(is_mutable_BidirectionalIterator_v<decltype(std::list<int>().begin())>);
-    static_assert(is_const_BidirectionalIterator_v<decltype(std::list<int>().cbegin())>);
+    {
+        static_assert(!is_BidirectionalIterator_v<mutable_ForwardIterator_t>);
+
+        static_assert(is_mutable_BidirectionalIterator_v<mutable_BidirectionalIterator_t>);
+        static_assert(is_const_BidirectionalIterator_v<const_BidirectionalIterator_t>);
+    }
 
     // Random Access Iterator
-    static_assert(is_mutable_RandomAccessIterator_v<int*>);
-    static_assert(is_const_RandomAccessIterator_v<const int*>);
+    {
+        static_assert(!is_RandomAccessIterator_v<int>);
+        static_assert(!is_RandomAccessIterator_v<mutable_BidirectionalIterator_t>);
 
-    static_assert(is_mutable_RandomAccessIterator_v<decltype(std::array<int, 10>().begin())>);
-    static_assert(is_const_RandomAccessIterator_v<decltype(std::array<int, 10>().cbegin())>);
+        static_assert(is_mutable_RandomAccessIterator_v<int*>);
+        static_assert(is_const_RandomAccessIterator_v<const int*>);
 
-    static_assert(is_mutable_RandomAccessIterator_v<decltype(std::vector<int>().begin())>);
-    static_assert(is_const_RandomAccessIterator_v<decltype(std::vector<int>().cbegin())>);
+        static_assert(is_mutable_RandomAccessIterator_v<decltype(std::vector<int>().begin())>);
+        static_assert(is_const_RandomAccessIterator_v<decltype(std::vector<int>().cbegin())>);
+    }
 }
